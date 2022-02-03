6f981320b6d8dc6978759dd938cb504937966d25
mikedyrhaug <dyrhaugm@gmail.com>
2022-02-03 09:28:39 -0500

    Added Date Range Picker

--- server.R
+++ server.R
@@ -2,15 +2,20 @@ library(shiny)
 
 server <- function(input, output) {
   output$debug <- renderPrint({
+    input$selected_date_range
    #input$selected_store
    #input$selected_sku
    #
    #sales_df[sales_df$scode == input$selected_store & sales_df$pcode == input$selected_sku, ]
   })
   output$ts_plot <- renderPlot({
+    min_date <- input$selected_date_range[1]
+    max_date <- input$selected_date_range[2]
     plot_df <- sales_df[sales_df$scode == input$selected_store & 
                           sales_df$pcode == input$selected_sku,
     ]
+    plot_df <- plot_df[plot_df$date >= min_date, ]
+    plot_df <- plot_df[plot_df$date <= max_date, ]
     autoplot(plot_df)
   })
 }
--- ui.R
+++ ui.R
@@ -9,6 +9,10 @@ selectInput(inputId = 'selected_sku',
             label = 'Select SKU', 
             choices = unique(sales_df$pcode)
 ),
+dateRangeInput(inputId = 'selected_date_range', label = 'Select Date Range',
+               start = min(sales_df$date), end = max(sales_df$date),
+               min = min(sales_df$date), max = max(sales_df$date)),
   plotOutput('ts_plot'),
       verbatimTextOutput('debug')
+
 )
